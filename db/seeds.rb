82ff7a62d8253cb345fe4f4bc57c6a9152349d3f
Frances <mynameislucah@yahoo.com>
2017-06-20 16:06:52 -0500

    db rollback

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,36 +1,28 @@
 Product.delete_all
-Product.create!(title: 'CoffeeScript',
+Product.create!(title: 'Marsh',
   description:
-    %{<p>
-        CoffeeScript is JavaScript done right. It provides all of JavaScript's
-	functionality wrapped in a cleaner, more succinct syntax. In the first
-	book on this exciting new language, CoffeeScript guru Trevor Burnham
-	shows you how to hold onto all the power and flexibility of JavaScript
-	while writing clearer, cleaner, and safer code.
-      </p>},
+      %{
+         Proust seems so full of food—crushed strawberries and madeleines,
+         tisanes and champagne—.
+      },
   image_url:   'gypsie.jpg',
   price: 36.00)
 # . . .
-Product.create!(title: 'Programming Ruby 1.9 & 2.0',
+Product.create!(title: 'Slake and Bake',
   description:
-    %{<p>
-        Ruby is the fastest growing and most exciting dynamic language
-        out there. If you need to get working programs delivered fast,
-        you should add Ruby to your toolbox.
-      </p>},
+    %{
+         Proust seems so full of food—crushed strawberries and madeleines,
+         tisanes and champagne—.
+      },
   image_url: 'gypsie.jpg',
   price: 49.95)
 # . . .
 
-Product.create!(title: 'Rails Test Prescriptions',
+Product.create!(title: 'Kittens',
   description:
-    %{<p>
-        <em>Rails Test Prescriptions</em> is a comprehensive guide to testing
-        Rails applications, covering Test-Driven Development from both a
-        theoretical perspective (why to test) and from a practical perspective
-        (how to test effectively). It covers the core Rails testing tools and
-        procedures for Rails 2 and Rails 3, and introduces popular add-ons,
-        including Cucumber, Shoulda, Machinist, Mocha, and Rcov.
-      </p>},
+    %{
+         Proust seems so full of food—crushed strawberries and madeleines,
+         tisanes and champagne—.
+      },
   image_url: 'gypsie.jpg',
   price: 34.95)
