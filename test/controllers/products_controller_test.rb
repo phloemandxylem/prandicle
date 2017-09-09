18092ea2faf19fe5cc1b3aa8f3d3366c3fd8b6e4
Frances <mynameislucah@yahoo.com>
2017-09-09 16:06:14 -0500

    added validations

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -3,28 +3,35 @@ require 'test_helper'
 class ProductsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @product = products(:one)
+    @update = {
+      title: 'Lorem Ipsem',
+      description: 'Colors are nice.',
+      image_url: 'lorem.jpg',
+      price: 19.98
+    }
   end
 
   test "should get index" do
-    get products_url
+    get :index
     assert_response :success
+    assert_not_nil assigns(:products)
   end
 
   test "should get new" do
-    get new_product_url
+    get :new
     assert_response :success
   end
 
   test "should create product" do
     assert_difference('Product.count') do
-      post products_url, params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title } }
-    end
+      post :create, product: @update
+  end
 
     assert_redirected_to product_url(Product.last)
   end
 
   test "should show product" do
-    get product_url(@product)
+    get :show
     assert_response :success
   end
 
@@ -34,15 +41,13 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update product" do
-    patch product_url(@product), params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title } }
-    assert_redirected_to product_url(@product)
+    patch :update, id: @product, product: @update
+    assert_redirected_to product_path(assigns(:product))
   end
 
   test "should destroy product" do
     assert_difference('Product.count', -1) do
       delete product_url(@product)
-    end
-
-    assert_redirected_to products_url
   end
+end 
 end
